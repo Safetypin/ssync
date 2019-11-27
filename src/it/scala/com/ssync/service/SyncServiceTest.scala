bd3bcff5262d3f03446f97a67ff2b186cd4cbad9
narkaah <nenyi.arkaah@gmail.com>
2019-11-27 00:13:42 +0000

    refactor: Addition of SyncService Integration Test

--- src/it/scala/com/ssync/service/SyncServiceTest.scala
+++ src/it/scala/com/ssync/service/SyncServiceTest.scala
@@ -1,7 +1,61 @@
 package com.ssync.service
 
-import org.scalatest.FunSuite
+import java.io.FileNotFoundException
 
-class SyncServiceTest extends FunSuite {
+import better.files.File
+import com.ssync.controllers.ItDataUtils._
+import com.ssync.controllers.FileToolUtils.getSeparator
+import com.ssync.models.{SettingSyncItem, Settings}
+import org.scalatest.Matchers._
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterEach, FlatSpec}
+
+class SyncServiceTest extends FlatSpec
+  with BeforeAndAfter
+  with BeforeAndAfterEach
+  with SyncService {
+
+  override def beforeEach {
+    setupTestResources
+  }
+
+  override def afterEach {
+    teardownTestResources
+  }
+
+  "run" should "throw an exception on first run" in {
+    intercept[FileNotFoundException](runSyncService())
+  }
+  it should "return a move all files from source-sub 1 to destination-sub" in {
+
+    val exampleSettings: Settings = Settings(
+      sourcePath,
+      destinationPath,
+      Array("*"),
+      Array(),
+      Seq(SettingSyncItem(
+        "sub folder 1",
+        "sub 1",
+        Array())
+      )
+    )
+
+    createSettingJson(settingsPath, exampleSettings)
+    runSyncService()
+    val subSourceDirectory = File(s"$sourcePath$getSeparator" + "sub 1")
+    val subDestinationDirectory = File(s"$destinationPath$getSeparator" + "sub 1")
+    subSourceDirectory.exists shouldEqual true
+    subSourceDirectory.isEmpty shouldEqual true
+    subDestinationDirectory.exists shouldEqual true
+    val subDestinationFiles = subDestinationDirectory.list.toList
+    subDestinationFiles.exists(f => f.name.equals("first.txt")) shouldEqual true
+    subDestinationFiles.filter(f => f.name.equals("first.txt")).length shouldEqual 1
+    subDestinationFiles.exists(f => f.name.equals("firstj.jpg")) shouldEqual true
+    subDestinationFiles.filter(f => f.name.equals("firstj.jpg")).length shouldEqual 1
+  }
+
+  private def createSettingJson(filename: String, settings: Settings) = {
+    val json = convertSettingsToJson(settings)
+      createFile(filename, json.toString)
+  }
 
 }
--- src/main/scala/com/ssync/service/SyncService.scala
+++ src/main/scala/com/ssync/service/SyncService.scala
@@ -1,11 +1,27 @@
 package com.ssync.service
 
 import com.ssync.controllers.{SettingsController, SyncController}
+import com.typesafe.scalalogging.LazyLogging
 
-trait SyncService extends SettingsController with SyncController {
+trait SyncService extends
+  SettingsController
+  with SyncController
+  with LazyLogging {
 
-  val syncItems = convertSettingSyncItemsToSyncItems(settings)
-  private val settings = loadSettings
+  def runSyncService() = {
+    val settings = loadSettings
+    val syncItems = convertSettingSyncItemsToSyncItems(settings)
+    val processedSyncItems = syncItems.map(item => processSyncItem(item))
+    val processedItems = processedSyncItems.length
+    val successfulProcessedItems = processedSyncItems.filter(_.isSuccess).length
+    val unsuccessfulProcessedItems = processedSyncItems.filter(_.isFailure).length
 
-
+    logger.info(s"$processedItems items processed.")
+    logger.info(s"$successfulProcessedItems items successfully processed.")
+    logger.info(s"$unsuccessfulProcessedItems items unsuccessfully processed.")
+//    syncItems
+//      .map(item => {
+//        cleanSyncItemSource(item)
+//      })
+  }
 }
